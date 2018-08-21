56fdbf3ce36942662871046e91873ce968ee3b14
Chris Donnelly <chris.donnelly@coxautoinc.com>
2018-08-21 15:22:26 -0500

    Adding OracleAdapter and wiring it up for testing.
    Also adding test suite SQL file in Oracle syntax.

---
+++ Dapper.Tests.Database/TestSuitesOracle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using Dapper.Database;
+using Xunit;
+
+#if !NETCOREAPP1_0
+using Oracle.ManagedDataAccess.Client;
+#endif
+
+namespace Dapper.Tests.Database
+{
+
+#if !NETCOREAPP1_0
+    [Trait("Provider", "Oracle")]
+    public partial class OracleTestSuite : TestSuite
+    {
+        private const string DbName = "test";
+        public static string ConnectionString =>
+            IsAppVeyor
+                ? $"User Id=DapperContribTests;Password=Password12!;Data Source=localhost:1521/{DbName}"
+                : $"User Id=DapperContribTests;Password=Password12!;Data Source=localhost:1521/ORCLPDB1.localdomain"; // FIXME need good service name like "test"
+
+        public override ISqlDatabase GetSqlDatabase()
+        {
+            if(_skip) throw new SkipTestException("Skipping Oracle Tests - no server.");
+            return new SqlDatabase(new StringConnectionService<OracleConnection>(ConnectionString));
+        }
+
+
+        public override Provider GetProvider() => Provider.Oracle;
+
+        private static readonly bool _skip;
+
+        private static readonly Regex CommandSeparator = new Regex("^/\r?\n", RegexOptions.Multiline);
+
+        static OracleTestSuite()
+        {
+            SqlMapper.AddTypeHandler<Guid>(new GuidTypeHandler());
+            try
+            {
+                using ( var connection = new OracleConnection(ConnectionString) )
+                {
+                    connection.Open();
+
+                    var awfile = File.ReadAllText(".\\Scripts\\oracleawlite.sql");
+
+                    // Because the Oracle driver does not support multiple statements in a single IDbCommand, we have to manually split the file.
+                    // The file is marked with lines with just forward slashes ("/"), which is the way SQL*Plus and other tools recognize the end of a command in such situations, so just use that.
+                    // (It also helps the ability to debug the script in SQL*Plus or another tool.)
+                    foreach (var command in CommandSeparator.Split(awfile))
+                    {
+                        // don't execute blank commands (e.g. last line)
+                        if (string.IsNullOrWhiteSpace(command))
+                            continue;
+                        // don't execute anything starting with a comment indicating use of SQL*Plus
+                        if (command.StartsWith("/*SQLPLUS*/", StringComparison.OrdinalIgnoreCase))
+                            continue;
+
+                        connection.Execute(command);
+                    }
+
+                    connection.Execute("delete from Person");
+                }
+            }
+
+            catch (SocketException e)
+            {
+                if (e.Message.Contains("No connection could be made because the target machine actively refused it"))
+                    _skip = true;
+                else
+                    throw;
+            }
+        }
+    }
+#endif
+
+}
