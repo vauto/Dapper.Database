eea7f60c1893a41f755963ad1854a06a63a58a51
Chris Donnelly <chris.donnelly@coxautoinc.com>
2020-01-29 09:57:55 -0600

    Re-adding separate ConcurrencyCheck methods.
    This reverts commit b51765a661df50decfc94dad681b7b40a7677211.

--- Dapper.Database/Adapters/SqlAdapter.cs
+++ Dapper.Database/Adapters/SqlAdapter.cs
@@ -501,10 +501,7 @@ namespace Dapper.Database.Adapters
             // Update failed, check for optimistic concurrency failure
             if (tableInfo.ConcurrencyCheckColumns.Any())
             {
-                if (Exists(connection, transaction, commandTimeout, tableInfo, entityToUpdate))
-                {
-                    throw new OptimisticConcurrencyException(tableInfo, entityToUpdate);
-                }
+                CheckConcurrency(connection, transaction, commandTimeout, tableInfo, entityToUpdate);
             }
 
             return false;
@@ -566,10 +563,7 @@ namespace Dapper.Database.Adapters
             // Update failed, check for optimistic concurrency failure
             if (tableInfo.ConcurrencyCheckColumns.Any())
             {
-                if (await ExistsAsync(connection, transaction, commandTimeout, tableInfo, entityToUpdate))
-                {
-                    throw new OptimisticConcurrencyException(tableInfo, entityToUpdate);
-                }
+                await CheckConcurrencyAsync(connection, transaction, commandTimeout, tableInfo, entityToUpdate);
             }
 
             return false;
@@ -769,6 +763,62 @@ namespace Dapper.Database.Adapters
             int? commandTimeout, TableInfo tableInfo, T entity)
         {
             return await connection.ExecuteScalarAsync<bool>(ExistsQuery(tableInfo), entity, transaction, commandTimeout);
+        }
+
+        #endregion
+
+        #region Concurrency Check Implementations
+
+        /// <summary>
+        ///     Checks whether the specified object exists, and if so, throws a concurrency exception.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="connection"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <param name="tableInfo"></param>
+        /// <param name="entity"></param>
+        /// <exception cref="OptimisticConcurrencyException">if the object exists</exception>
+        /// <remarks>
+        /// Base implementation currently assumes the caller performed an operation that resulted in possible concurrency failure,
+        /// and does not attempt to compare the values again.
+        /// </remarks>
+        protected virtual void CheckConcurrency<T>(IDbConnection connection, IDbTransaction transaction, int? commandTimeout,
+            TableInfo tableInfo, T entity)
+        {
+            if (!tableInfo.ConcurrencyCheckColumns.Any())
+                return;
+
+            if (Exists(connection, transaction, commandTimeout, tableInfo, entity))
+            {
+                throw new OptimisticConcurrencyException(tableInfo, entity);
+            }
+        }
+
+        /// <summary>
+        ///     Checks whether the specified object exists, and if so, throws a concurrency exception.
+        /// </summary>
+        /// <typeparam name="T">the entity type</typeparam>
+        /// <param name="connection">Open SqlConnection</param>
+        /// <param name="transaction">The transaction to run under, null (the default) if none</param>
+        /// <param name="commandTimeout">Number of seconds before command execution timeout</param>
+        /// <param name="tableInfo">table information about the entity</param>
+        /// <param name="entity">Entity to check</param>
+        /// <exception cref="OptimisticConcurrencyException">if the object exists</exception>
+        /// <remarks>
+        /// Base implementation currently assumes the caller performed an operation that resulted in possible concurrency failure,
+        /// and does not attempt to compare the values again.
+        /// </remarks>
+        protected virtual async Task CheckConcurrencyAsync<T>(IDbConnection connection, IDbTransaction transaction,
+            int? commandTimeout, TableInfo tableInfo, T entity)
+        {
+            if (!tableInfo.ConcurrencyCheckColumns.Any())
+                return;
+
+            if (await ExistsAsync(connection, transaction, commandTimeout, tableInfo, entity))
+            {
+                throw new OptimisticConcurrencyException(tableInfo, entity);
+            }
         }
 
         #endregion
